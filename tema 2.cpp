25044404c1343f0ce98253f12f191ffde85d1f19
VelicuAlexandru <44846384+VelicuAlexandru@users.noreply.github.com>
2018-11-22 11:34:02 +0200

    Update tema 2.cpp

--- tema 2.cpp
+++ tema 2.cpp
@@ -2,35 +2,34 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int n,a,i,j,x,p=0,u;
-	cout << " introduceti numarul de liste:  " << endl ;
+	int n, a, i, j, x, p = 0, u;
+	cout << " introduceti numarul de liste:  " << endl;
 	cin >> n;
 	int **v;
 	int *h = new int[n];
-	v= new int*[n];
-	for (i = 1; i <= n; i++) {
+	v = new int*[n];
+	for (i = 0; i < n; i++) {
 		cout << "cate elemente are lista ?" << endl;
 		cin >> a;
 		v[i] = new int[a];
 		h[i] = a;
 		cout << "introduceti elementele " << endl;
-		for (j = 1; j <= a; j++) {
+		for (j = 0; j < a; j++) {
 			cin >> x;
 			p++;
 			v[i][j] = x;
 		}
-
 	}
-	int f = 1, aux;
+	int f = 0, aux;
 	int *z = new int[p];
-	for (i = 1; i <= n; i++) {
-		for (j = 1; j <= h[i]; j++) {
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < h[i]; j++) {
 			z[f] = v[i][j];
 			f++;
 		}
 	}
-	for (i = 1; i <= p; i++) {
-		for (j = i; j <= p; j++) {
+	for (i = 0; i < p; i++) {
+		for (j = i; j < p; j++) {
 			if (z[i] > z[j]) {
 				aux = z[i];
 				z[i] = z[j];
@@ -40,10 +39,12 @@ int main() {
 	}
 
 	cout << "numerele sortate sunt : " << endl;
-	for (i = 1; i <= p; i++) {
+	for (i = 0; i < p; i++) {
 		cout << z[i] << endl;
 	}
-	system ("pause");
-
+	delete[]v;
+	delete[] h;
+	delete[] z;
+	system("pause");
 	return 0;
 }
